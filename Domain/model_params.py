fb3a4ea00ef137c0fdbccc14e9e241a45900ff44
James <james.m.evansmba@gmail,com>
2022-05-20 15:49:38 -0500

    small changes

---
+++ Domain/model_params.py
@@ -0,0 +1,6 @@
+class model_paramns:
+
+    def __init__(self,clusters,scaler):
+        self.clusters = clusters
+        self.scaler = scaler
+
---
+++ MLProcess/clustering.py
@@ -0,0 +1,4 @@
+class clustering:
+
+    def __init__(self):
+        pass
